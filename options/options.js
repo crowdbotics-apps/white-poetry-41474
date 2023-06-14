f0538371f9d101a7833f9489236072a239722fa9
Crowdbotics <team@crowdbotics.com>
2023-06-14 14:13:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "white-poetry-41474",
+  url: "https://white-poetry-41474.botics.co",
+  api: "https://white-poetry-41474.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
